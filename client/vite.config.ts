b99fda8201d73232aedd560c77959dd7671114d9
Altaf <naturespack1@gmail.com>
2026-07-12 02:05:30 +0530

    link api to ui

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://rollsco-server-fb6ahayts-rollsco.vercel.app',
         changeOrigin: true
       }
     }
